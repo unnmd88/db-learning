b66dcec01578e241606898f34ab6e4190fccdcc8
auser <unnmd88@gmail.com>
2025-10-22 01:32:09 +0300

    add postgres config and settings

---
+++ config.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = True
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    db: DatabaseConfig
+
+
+settings = Settings()
+
+if __name__ == '__main__':
+    print(settings)
---
+++ queries.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from sqlalchemy import text
+
+from database_api import db_api
+
+
+async def get_all():
+    async with db_api.engine.connect() as conn:
+        res = await conn.execute(text(f'SELECT * FROM first'))
+        print(f'res: {res.all()}')
+
+
+async def insert_data():
+    stmt = text(
+        """INSERT INTO first (id, name) VALUES (4, 'Pimmo');"""
+    )
+    async with db_api.engine.connect() as conn:
+        res = await conn.execute(stmt)
+        await conn.commit()
+        # print(f'res: {res.first()}')
+
+
+
+async def main():
+    await get_all()
+    # await insert_data()
+    # await get_first()
+
+
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
